6c6dbe8d9882e08a37655d3c5d71cea24012d515
Beta Kuang <beta.kuang@gmail.com>
2018-06-27 21:17:19 +0800

    add example skeleton

---
+++ example/example.go
@@ -0,0 +1,163 @@
+// Copyright (c) 2018 Beta Kuang
+//
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/beta/glfw"
+	"github.com/beta/nanovgo"
+	"github.com/go-gl/gl/v3.2-core/gl"
+)
+
+// DemoMSAA specifies whether to enable MSAA.
+const DemoMSAA = false
+
+func errorCallback(err glfw.Error, desc string) {
+	fmt.Printf("GLFW error: %d: %s\n", int(err), desc)
+}
+
+var (
+	blowup     bool
+	screenshot bool
+	premult    bool
+)
+
+func key(win *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierFlag) {
+	if key == glfw.KeyEscape && action == glfw.Press {
+		win.SetShouldClose(true)
+	}
+	if key == glfw.KeySpace && action == glfw.Press {
+		blowup = !blowup
+	}
+	if key == glfw.KeyS && action == glfw.Press {
+		screenshot = true
+	}
+	if key == glfw.KeyP && action == glfw.Press {
+		premult = !premult
+	}
+}
+
+func main() {
+	var ctx = glfw.Init()
+	if ctx == nil {
+		panic("failed to init GLFW")
+	}
+	defer ctx.Terminate()
+
+	var fps = NewPerfGraph(GraphRenderFPS, "Frame Time")
+	var cpuGraph = NewPerfGraph(GraphRenderMS, "CPU Time")
+
+	ctx.SetErrorCallback(errorCallback)
+
+	ctx.WindowHint(glfw.ContextVersionMajor, 3)
+	ctx.WindowHint(glfw.ContextVersionMinor, 2)
+	ctx.WindowHintBool(glfw.OpenGLForwardCompat, true)
+	ctx.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	ctx.WindowHintBool(glfw.OpenGLDebugContext, true)
+
+	if DemoMSAA {
+		ctx.WindowHint(glfw.Samples, 4)
+	}
+
+	var win = ctx.CreateWindow(1000, 600, "NanoVGo", nil, nil)
+	if win == nil {
+		panic("failed to create window")
+	}
+	defer win.Destroy()
+
+	win.SetKeyCallback(key)
+	ctx.MakeContextCurrent(win)
+
+	var vg *nanovgo.Context
+	if DemoMSAA {
+		vg = nanovgo.CreateContext(nanovgo.StencilStrokes | nanovgo.Debug)
+	} else {
+		vg = nanovgo.CreateContext(nanovgo.Antialias | nanovgo.StencilStrokes | nanovgo.Debug)
+	}
+	if vg == nil {
+		panic("failed to init NanoVGo")
+	}
+	defer vg.Delete()
+
+	demo, err := loadDemoData(vg)
+	if err != nil {
+		panic("failed to load demo data")
+	}
+
+	ctx.SwapInterval(0)
+	ctx.SetTime(0)
+	var prevt = ctx.GetTime()
+	var cpuTime float64
+
+	// Use github.com/go-gl/gl for OpenGL APIs.
+	err = gl.Init()
+	if err != nil {
+		panic("failed to init OpenGL")
+	}
+
+	for !win.ShouldClose() {
+		var t = ctx.GetTime()
+		var dt = t - prevt
+		prevt = t
+
+		var mx, my = win.GetCursorPos()
+		var winWidth, winHeight = win.GetSize()
+		var fbWidth, fbHeight = win.GetFramebufferSize()
+		// Calculate pixel ratio for Hi-DPI devices.
+		var pxRatio = float64(fbWidth) / float64(winWidth)
+
+		gl.Viewport(0, 0, int32(fbWidth), int32(fbHeight))
+		if premult {
+			gl.ClearColor(0, 0, 0, 0)
+		} else {
+			gl.ClearColor(0.3, 0.3, 0.32, 1.0)
+		}
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
+
+		vg.BeginFrame(float32(winWidth), float32(winHeight), float32(pxRatio))
+
+		demo.render(vg, mx, my, float64(winWidth), float64(winHeight), t, blowup)
+
+		fps.Render(vg, 5, 5)
+		cpuGraph.Render(vg, 5+200+5, 5)
+
+		vg.EndFrame()
+
+		// Measure the CPU time taken excluding swap buffers (as the swap may
+		// wait for GPU).
+		cpuTime = ctx.GetTime() - t
+
+		fps.Update(dt)
+		cpuGraph.Update(cpuTime)
+
+		if screenshot {
+			screenshot = false
+			saveScreenShot(fbWidth, fbHeight, premult, "dump.png")
+		}
+
+		win.SwapBuffers()
+		ctx.PollEvents()
+	}
+
+	demo.free()
+
+	fmt.Printf("Average Frame Time: %.2f ms\n", fps.Average()*1000.0)
+	fmt.Printf("          CPU Time: %.2f ms\n", cpuGraph.Average()*1000.0)
+}
---
+++ example/perf.go
@@ -0,0 +1,149 @@
+// Copyright (c) 2018 Beta Kuang
+//
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/beta/nanovgo"
+)
+
+// GraphHistoryCount is the length of historical values to be stored in a
+// performance graph.
+const GraphHistoryCount = 100
+
+// NewPerfGraph creates and returns a new performance graph.
+func NewPerfGraph(style PerfGraphStyle, name string) *PerfGraph {
+	return &PerfGraph{
+		style: style,
+		name:  name,
+	}
+}
+
+// PerfGraph stores and analyzes the performance values.
+type PerfGraph struct {
+	style  PerfGraphStyle
+	name   string
+	values [GraphHistoryCount]float64
+	head   int
+}
+
+// PerfGraphStyle specifies the rendering style of a performance graph.
+type PerfGraphStyle int
+
+// PerfGraph styles.
+const (
+	GraphRenderFPS PerfGraphStyle = iota
+	GraphRenderMS
+	GraphRenderPercent
+)
+
+// Update records the frameTime into fps.
+func (fps *PerfGraph) Update(frameTime float64) {
+	fps.head = (fps.head + 1) % GraphHistoryCount
+	fps.values[fps.head] = frameTime
+}
+
+// Average returns the average FPS.
+func (fps *PerfGraph) Average() float64 {
+	var avg float64
+	for i := 0; i < GraphHistoryCount; i++ {
+		avg += fps.values[i]
+	}
+	return avg / float64(GraphHistoryCount)
+}
+
+// Render renders the performance graph onto location (x,y) of ctx.
+func (fps *PerfGraph) Render(ctx *nanovgo.Context, x, y float32) {
+	var avg = fps.Average()
+	var width, height float32 = 200, 35
+
+	ctx.BeginPath()
+	ctx.Rect(x, y, width, height)
+	ctx.FillColor(nanovgo.RGBA(0, 0, 0, 128))
+	ctx.Fill()
+
+	ctx.BeginPath()
+	ctx.MoveTo(x, y+height)
+	if fps.style == GraphRenderFPS {
+		for i := 0; i < GraphHistoryCount; i++ {
+			var v = 1.0 / (0.00001 + fps.values[(fps.head+i)/GraphHistoryCount])
+			if v > 80.0 {
+				v = 80.0
+			}
+			var vx = x + (float32(i)/(GraphHistoryCount-1))*width
+			var vy = y + height - (float32(v/80.0) * height)
+			ctx.LineTo(vx, vy)
+		}
+	} else if fps.style == GraphRenderPercent {
+		for i := 0; i < GraphHistoryCount; i++ {
+			var v = fps.values[(fps.head+i)%GraphHistoryCount] * 1.0
+			if v > 100.0 {
+				v = 100.0
+			}
+			var vx = x + (float32(i)/(GraphHistoryCount-1))*width
+			var vy = y + height - (float32(v/100.0) * height)
+			ctx.LineTo(vx, vy)
+		}
+	} else {
+		for i := 0; i < GraphHistoryCount; i++ {
+			var v = fps.values[(fps.head+i)%GraphHistoryCount] * 1000.0
+			if v > 20.0 {
+				v = 20.0
+			}
+			var vx = x + (float32(i)/(GraphHistoryCount-1))*width
+			var vy = y + height - (float32(v/20.0) * height)
+			ctx.LineTo(vx, vy)
+		}
+	}
+	ctx.LineTo(x+width, y+height)
+	ctx.FillColor(nanovgo.RGBA(255, 192, 0, 128))
+	ctx.Fill()
+
+	ctx.FontFace("sans")
+
+	if len(fps.name) > 0 {
+		ctx.FontSize(14.0)
+		ctx.TextAlign(nanovgo.AlignLeft | nanovgo.AlignTop)
+		ctx.FillColor(nanovgo.RGBA(240, 240, 240, 192))
+		ctx.Text(x+3, y+1, fps.name)
+	}
+
+	if fps.style == GraphRenderFPS {
+		ctx.FontSize(18.0)
+		ctx.TextAlign(nanovgo.AlignRight | nanovgo.AlignTop)
+		ctx.FillColor(nanovgo.RGBA(240, 240, 240, 255))
+		ctx.Text(x+width-3, y+1, fmt.Sprintf("%.2f FPS", 1.0/avg))
+
+		ctx.FontSize(15.0)
+		ctx.TextAlign(nanovgo.AlignRight | nanovgo.AlignBottom)
+		ctx.FillColor(nanovgo.RGBA(240, 240, 240, 160))
+		ctx.Text(x+width-3, y+height-1, fmt.Sprintf("%.2f ms", avg*1000.0))
+	} else if fps.style == GraphRenderPercent {
+		ctx.FontSize(18.0)
+		ctx.TextAlign(nanovgo.AlignRight | nanovgo.AlignTop)
+		ctx.FillColor(nanovgo.RGBA(240, 240, 240, 255))
+		ctx.Text(x+width-3, y+1, fmt.Sprintf("%.1f %%", avg*1.0))
+	} else {
+		ctx.FontSize(18.0)
+		ctx.TextAlign(nanovgo.AlignRight | nanovgo.AlignTop)
+		ctx.FillColor(nanovgo.RGBA(240, 240, 240, 255))
+		ctx.Text(x+width-3, y+1, fmt.Sprintf("%.2f ms", avg*1000.0))
+	}
+}
